01b6249585c6d48338713223719915a2815c0609
Haider Ali <haider.ali3737@gmail.com>
2021-03-26 04:19:08 -0700

    Header is ready.

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,14 @@
+// Top bar function to close button 
 function closeBtn(){
     let topBar = document.getElementById("topBar");
     topBar.remove();
+}
+// Navigation menu toggler function
+function togglemenu(){
+    let x = document.getElementById("navBar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
 }
